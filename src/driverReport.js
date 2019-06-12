5efd1a1bb475cdb80b41eefd32cbbcc92df1da6c
Tolu Adesina <tadesina90@gmail.com>
2019-06-12 16:09:46 +0100

    driverReport file created for seperating report function

---
+++ src/driverReport.js
@@ -0,0 +1,11 @@
+const { getTrips, getDriver, getVehicle } = require('./api/index');
+/**
+ * This function should return the data for drivers in the specified format
+ * Don't forget to write tests
+ *
+ * @returns {any} Driver report data
+ */
+async function driverReport() {
+
+}
+module.exports = driverReport;
