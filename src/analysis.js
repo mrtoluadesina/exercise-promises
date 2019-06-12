4aa52050b9182936874816dba59bca22e2191f7b
Tolu Adesina <tadesina90@gmail.com>
2019-06-12 16:06:16 +0100

    Changing my file structure - seperating analysis and driver report into individual files and then requiring in index.js

---
+++ src/analysis.js
@@ -0,0 +1,12 @@
+const { getTrips, getDriver } = require('./api/index');
+/**
+ * This function should return the trip data analysis
+ * Don't forget to write tests
+ *
+ * @returns {any} Trip data analysis
+ */
+async function analysis() {
+  
+}
+
+module.exports = analysis;
