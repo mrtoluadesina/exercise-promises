0429c7568b47ee5073b1ae1a96fc3e1c1f986ea6
Tolu Adesina <tadesina90@gmail.com>
2019-06-12 09:42:24 +0100

    Refactored codebase to have spaces and cleaner constructs, removed unnecessary variables

--- src/index.js
+++ src/index.js
@@ -44,13 +44,27 @@ async function analysis() {
   // get total non cash billed total
   let nonCashBilledTotal = Number(billedTotal.toFixed(2)) - cashBilledTotal;
 
-  // get number of drivers with more than one vehicle
-  let noOfDriversWithMoreThanOneVehicle = 0;
-  for (let driver of uniqueDriverID.values()) {
-    getDriver(driver).then(data => {
-      if(data.vehicleID.length > 1) noOfDriversWithMoreThanOneVehicle++;
-    }).catch(err => {});
-  }
+  //get driver information
+  const driverIDs = [...drivers.keys()];
+
+  const driverPromises = driverIDs.map(async driverID => {
+    try {
+      const data = await getDriver(driverID);
+      
+      return {
+        ...data,
+        driverID
+      };
+    } catch {
+      return;
+    }
+  });
+
+  const driversData = await Promise.all(driverPromises);
+
+  const noOfDriversWithMoreThanOneVehicle = driversData.filter(driver => {
+    return driver && driver.vehicleID.length > 1;
+  }).length;
 
   // get the most trips by a driver
   const driverArray = [...drivers];
@@ -86,7 +100,6 @@ async function analysis() {
     highestEarningDriver: highestEarningDriver
   };
 }
-analysis()
 /**
  * This function should return the data for drivers in the specified format
  * Don't forget to write tests
@@ -94,12 +107,13 @@ analysis()
  * @returns {any} Driver report data
  */
 async function driverReport() {
-  const report = [];
   const uniqueDriverID = new Set();
   const driverDetails = new Map();
   const allTrips = await getTrips();
+
   allTrips.forEach(trip => {
-    uniqueDriverID.add(allTrips.driverID);
+    uniqueDriverID.add(trip.driverID);
+    
     const obj = {
       user: trip.user.name,
       created: trip.created,
@@ -108,135 +122,68 @@ async function driverReport() {
       billed: trip.billedAmount,
       isCash: trip.isCash
     };
+
     if (!driverDetails.has(trip.driverID)) {
       if (trip.isCash) {
         driverDetails.set(trip.driverID, {
           noOfCashTrips: 1,
           noOfNonCashTrips: 0,
-          cashAmountEarned: Number(String(trip.billedAmount).replace(/,/g, '')),
-          nonCashAmountEarned: 0,
+          totalCashAmount: Number(String(trip.billedAmount).replace(/,/g, '')),
+          totalNonCashAmount: 0,
           trips: [obj]
         });
       } else {
         driverDetails.set(trip.driverID, {
           noOfCashTrips: 0,
           noOfNonCashTrips: 1,
-          cashAmountEarned: 0,
-          nonCashAmountEarned: Number(
+          totalCashAmount: 0,
+          totalNonCashAmount: Number(
             String(trip.billedAmount).replace(/,/g, '')
           ),
           trips: [obj]
         });
       }
+
     } else {
       let {
         noOfCashTrips,
         noOfNonCashTrips,
-        cashAmountEarned,
-        nonCashAmountEarned,
+        totalCashAmount,
+        totalNonCashAmount,
         trips
       } = driverDetails.get(trip.driverID);
+
       if (trip.isCash) {
         noOfCashTrips++;
-        cashAmountEarned += Number(String(trip.billedAmount).replace(/,/g, ''));
+        totalCashAmount += Number(String(trip.billedAmount).replace(/,/g, ''));
+
         trips.push(obj);
+        
         driverDetails.set(trip.driverID, {
           noOfCashTrips,
           noOfNonCashTrips,
-          cashAmountEarned,
-          nonCashAmountEarned,
+          totalCashAmount,
+          totalNonCashAmount,
           trips
         });
       } else {
         noOfNonCashTrips++;
-        nonCashAmountEarned += Number(
+        totalNonCashAmount += Number(
           String(trip.billedAmount).replace(/,/g, '')
         );
         trips.push(obj);
         driverDetails.set(trip.driverID, {
           noOfCashTrips,
           noOfNonCashTrips,
-          cashAmountEarned,
-          nonCashAmountEarned,
+          totalCashAmount,
+          totalNonCashAmount,
           trips
         });
       }
     }
   });
 
-  // try {
-  //   for (let [key, value] of driverDetails) {
-  //     const { name, phone, vehicleID } = await getDriver(key);
-  //     const vehicleDetails = [];
-  //     for (let id of vehicleID) {
-  //       const { plate, manufacturer } = await getVehicle(id);
-  //       const details = {
-  //         plate: plate,
-  //         manufacturer: manufacturer
-  //       };
-  //       vehicleDetails.push(details);
-  //     }
-
-  //     //Structure Output
-  //     const driver = {};
-  //     driver.fullName = name;
-  //     driver.id = key;
-  //     driver.phone = phone;
-  //     driver.noOfTrips = value.noOfCashTrips + value.noOfNonCashTrips;
-  //     driver.noOfVehicles = vehicleID.length;
-  //     driver.vehicles = vehicleDetails;
-  //     driver.noOfCashTrips = value.noOfCashTrips;
-  //     driver.noOfNonCashTrips = value.noOfNonCashTrips;
-  //     driver.totalAmountEarned =
-  //       Number(value.cashAmountEarned.toFixed(2)) +
-  //       Number(value.nonCashAmountEarned.toFixed(2));
-  //     driver.totalCashAmount = Number(value.cashAmountEarned.toFixed(2));
-  //     driver.totalNonCashAmount = Number(value.nonCashAmountEarned.toFixed(2));
-  //     driver.trips = value.trips;
-
-  //     //push into report
-  //     report.push(driver);
-  //   }
-  // } catch (err) {}
-  for (let [key, value] of driverDetails) {
-    getDriver(key).then(data => {
-      const vehicleDetails = [];
-      console.log(data.name);
-      for (let id of data.vehicleID) {
-        getVehicle(id).then(item => {
-          const details = {
-            plate: item.plate,
-            manufacturer: item.manufacturer
-          };
-          vehicleDetails.push(details);
-        })
-      }
-
-      //Structure Output
-      const driver = {
-        fullName: data.name,
-        id: key,
-        phone: data.phone,
-        noOfTrips: value.noOfCashTrips + value.noOfNonCashTrips,
-        noOfVehicles: data.vehicleID.length,
-        vehicles: vehicleDetails,
-        noOfCashTrips: value.noOfCashTrips,
-        noOfNonCashTrips: value.noOfNonCashTrips,
-        totalAmountEarned:
-        Number(value.cashAmountEarned.toFixed(2)) +
-        Number(value.nonCashAmountEarned.toFixed(2)),
-        totalCashAmount: Number(value.cashAmountEarned.toFixed(2)),
-        totalNonCashAmount: Number(value.nonCashAmountEarned.toFixed(2)),
-        trips = value.trips,
-      };
-
-      //push into report
-      report.push(driver);
-    }).catch(err => {})
-  }
-
-  // console.log(report);
+  
   return report;
 }
-driverReport();
 module.exports = { analysis, driverReport };
